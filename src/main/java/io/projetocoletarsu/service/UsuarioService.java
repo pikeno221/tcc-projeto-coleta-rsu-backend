0727e2793a21fca190b04bd0f712e7ec3488412e
pikeno221 <gabriellgomesbh@gmail.com>
2019-09-16 21:04:58 -0300

    refactor exceptions + atualizar usuario

--- src/main/java/io/projetocoletarsu/service/UsuarioService.java
+++ src/main/java/io/projetocoletarsu/service/UsuarioService.java
@@ -1,10 +1,10 @@
 package io.projetocoletarsu.service;
 
 import io.projetocoletarsu.controller.UsuarioApiController;
-import io.projetocoletarsu.exception.BDException;
+import io.projetocoletarsu.exception.ApiException;
 import io.projetocoletarsu.model.Usuario;
-import io.projetocoletarsu.model.retorno.RetornoUsuario;
 import io.projetocoletarsu.model.retorno.RetornoTodosUsuarios;
+import io.projetocoletarsu.model.retorno.RetornoUsuario;
 import io.projetocoletarsu.repository.UsuarioRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +22,7 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository repository;
 
-    public RetornoUsuario criarUsuario(Usuario usuario) throws BDException {
+    public RetornoUsuario criarUsuario(Usuario usuario) throws ApiException {
         RetornoUsuario retorno = new RetornoUsuario();
 
         try {
@@ -41,12 +41,12 @@ public class UsuarioService {
 
         } catch (Exception e) {
             log.error("Error ao persistir os dados", e);
-            throw new BDException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro ao persistir dados.");
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro ao persistir dados.");
         }
 
     }
 
-    public RetornoTodosUsuarios buscarTodosUsuarios() throws BDException {
+    public RetornoTodosUsuarios buscarTodosUsuarios() throws ApiException {
         RetornoTodosUsuarios retorno = new RetornoTodosUsuarios();
 
         try {
@@ -63,36 +63,93 @@ public class UsuarioService {
 
         } catch (Exception e) {
             log.error("Erro ao buscar os dados", e);
-            throw new BDException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro ao buscar os dados.");
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro ao buscar os dados.");
         }
 
     }
 
-    public RetornoUsuario buscarUsuarioPorId(Integer idUsuario) throws BDException {
+    public RetornoUsuario buscarUsuarioPorId(Integer idUsuario) throws ApiException {
         RetornoUsuario retorno = new RetornoUsuario();
+        Optional<Usuario> usuario;
 
         try {
-            Optional<Usuario> usuario = repository.findById(idUsuario);
 
-            if (usuario.isPresent()) {
-                retorno.setUsuario(usuario.get());
-                retorno.setMensagem("Sucesso ao buscar usuario");
+            usuario = repository.findById(idUsuario);
+        } catch (Exception e) {
+            log.error("Erro ao buscar os dados", e);
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro ao buscar os dados.");
+
+        }
+
+        if (usuario.isPresent()) {
+            retorno.setUsuario(usuario.get());
+            retorno.setMensagem("Sucesso ao buscar usuario");
+            retorno.setSucesso(true);
+        } else {
+            retorno.setUsuario(null);
+            retorno.setMensagem("Usuario Nao Encontrado");
+            retorno.setSucesso(false);
+        }
+
+        return retorno;
+
+    }
+
+
+    public RetornoUsuario atualizarUsuario(Integer idUsuario, Usuario usuario) throws ApiException {
+        try {
+            RetornoUsuario retorno;
+
+            retorno = buscarUsuarioPorId(idUsuario);
+
+            if (retorno.isSucesso()) {
+                retorno.setUsuario(repository.save(setaValoresAtualizacaoUsuario(retorno.getUsuario(), usuario)));
                 retorno.setSucesso(true);
-            } else {
-                retorno.setUsuario(null);
-                retorno.setMensagem("Usuario Nao Encontrado");
-                retorno.setSucesso(false);
+                retorno.setMensagem("Usuario atualizado com sucesso");
             }
 
             return retorno;
 
-        } catch (Exception e) {
-            log.error("Erro ao buscar os dados", e);
-            throw new BDException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro ao buscar os dados.");
 
+        } catch (Exception e) {
+            log.error("Erro ao atualizar os dados", e);
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro ao buscar os dados.");
         }
+    }
+
+    private Usuario setaValoresAtualizacaoUsuario(Usuario usuarioBanco, Usuario usuarioDto) {
+
+        if (usuarioDto.getNomeCompleto() != null && !usuarioDto.getNomeCompleto().isEmpty())
+            usuarioBanco.setNomeCompleto(usuarioDto.getNomeCompleto());
+
+        if (usuarioDto.getCelular() != null && !usuarioDto.getCelular().isEmpty())
+            usuarioBanco.setCelular(usuarioDto.getCelular());
+
+
+        if (usuarioDto.getEmail() != null && !usuarioDto.getEmail().isEmpty())
+            usuarioBanco.setEmail(usuarioDto.getEmail());
+
+
+        if (usuarioDto.getCpf() != null && !usuarioDto.getCpf().isEmpty())
+            usuarioBanco.setCpf(usuarioDto.getCpf());
+
+        if (usuarioDto.getEndereco() != null && !usuarioDto.getEndereco().isEmpty())
+            usuarioBanco.setEndereco(usuarioDto.getEndereco());
+
+        if (usuarioDto.getSenha() != null && !usuarioDto.getSenha().isEmpty())
+            usuarioBanco.setSenha(usuarioDto.getSenha());
+
+        if (usuarioDto.getStatusUsuario() != null)
+            usuarioBanco.setStatusUsuario(usuarioDto.getStatusUsuario());
+
+        if (usuarioDto.getSenha() != null && !usuarioDto.getSenha().isEmpty())
+            usuarioBanco.setSenha(usuarioDto.getSenha());
+
+        return usuarioBanco;
 
     }
+
+
     private boolean usuarioValidoParaInsercao(Usuario usuario) {
         return buscarUsuarioPorCpf(usuario.getCpf()) == null && buscarUsuarioPorEmail(usuario.getEmail()) == null && buscarUsuarioPorTelefone(usuario.getCelular()) == null;
     }
@@ -108,7 +165,6 @@ public class UsuarioService {
     public Usuario buscarUsuarioPorEmail(String email) {
         return repository.findByEmail(email);
     }
-
 
 
 }
